de7ae8dfbe97308ec25ba234ba0dc164b63d5b5b
panka <pankajkr.jo@gmail.com>
2020-12-31 17:38:32 +0530

    Space

--- src/main/java/com/mkyong/config/SpringSecurityConfig.java
+++ src/main/java/com/mkyong/config/SpringSecurityConfig.java
@@ -26,7 +26,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-          http
+           http
                 //HTTP Basic authentication
                 .httpBasic()
                 .and()
